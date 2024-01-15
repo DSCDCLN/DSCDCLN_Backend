fee53ab94e857eab11fbe5d78b3f81926bbd6199
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-01-15 14:39:39 +0900

    init

---
+++ src/main/java/com/dscdcln/DscdclnApplication.java
@@ -0,0 +1,13 @@
+package com.dscdcln;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscdclnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DscdclnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dscdcln/DscdclnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dscdcln;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscdclnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

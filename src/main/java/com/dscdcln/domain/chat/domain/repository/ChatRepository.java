5efbcd51244edbf0c32bf76901710b939fee2ee5
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-01-17 18:27:39 +0900

    Remove :: File

--- src/main/java/com/dscdcln/domain/chat/domain/repository/ChatRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dscdcln.domain.chat.domain.repository;
-
-import com.dscdcln.domain.chat.domain.Chat;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatRepository extends JpaRepository<Chat, Long>, ChatRepositoryCustom {
-}
--- src/main/java/com/dscdcln/domain/chat/domain/repository/ChatRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.dscdcln.domain.chat.domain.repository;
-
-import com.dscdcln.domain.chat.dto.ResponseChat;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-public interface ChatRepositoryCustom {
-    Page<ResponseChat> getChats(Pageable pageable);
-}
